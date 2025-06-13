980d99ac44a7421f193d61b516296d7ee657537d
angella1709 <159751231+angella1709@users.noreply.github.com>
2025-06-13 13:18:19 +0500

    3

--- main-service/src/main/resources/02_import.sql
+++ main-service/src/main/resources/02_import.sql
@@ -6,7 +6,6 @@ INSERT INTO categories(name) VALUES ('Концерты');
 INSERT INTO categories(name) VALUES ('Театр');
 INSERT INTO categories(name) VALUES ('Обучение');
 INSERT INTO categories(name) VALUES ('Кино');
-INSERT INTO categories(name) VALUES ('Фестивали');
 INSERT INTO categories(name) VALUES ('Экскурсии');
 INSERT INTO categories(name) VALUES ('Стендапы');
 INSERT INTO categories(name) VALUES ('Квесты');
